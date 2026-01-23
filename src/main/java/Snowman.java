1ddd20693b54176d4c6267475ebf69f35dc38f4d
FacelessSnowman <khozheming2905@gmail.com>
2026-01-23 17:22:35 +0800

    Level-0

---
+++ src/main/java/Snowman.java
@@ -0,0 +1,18 @@
+public class Snowman {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                ____________________________________________________________
+                Hello! I'm snowman
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """);
+    }
+}
