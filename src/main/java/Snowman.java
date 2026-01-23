013dc105487b86271eae2376f0f770992d1f85aa
FacelessSnowman <khozheming2905@gmail.com>
2026-01-23 17:58:09 +0800

    Level-2

--- src/main/java/Snowman.java
+++ src/main/java/Snowman.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Snowman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String line = "____________________________________________________________";
+        String nextLine = "\n";
+
+        ArrayList<String> enterList = new ArrayList<>(100);
+
         System.out.println("""
                 ____________________________________________________________
                 Hello! I'm snowman
@@ -14,17 +22,25 @@ public class Snowman {
             String input = scanner.nextLine();
 
             if(input.equals("bye")) {
-                System.out.println("""
-                        ____________________________________________________________
-                        Bye. Hope to see you again soon!
-                        ____________________________________________________________
-                        """);
+                System.out.println(line + nextLine
+                        + "Bye. Hope to see you again soon!\n"
+                        + line);
                 break;
             }
 
-            System.out.print("____________________________________________________________\n"
-            + input + "\n____________________________________________________________\n");
+            if(input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < enterList.size(); i++) {
+                    System.out.println((i + 1) + ". " + enterList.get(i));
+                }
+                System.out.println(line);
+            } else {
+                enterList.add(input);
 
+                System.out.print(line + nextLine
+                        + "added: " + input + "\n"
+                        + line + nextLine);
+            }
         }
     }
 }
